22fe482cfacab7320708cb2fd41a9c016affe4fc
Tobias Engbroks <tengbroks@gmx.de>
2023-12-02 20:08:03 +0100

    Aufgaben bearbeitet

--- aufgabe.py
+++ aufgabe.py
@@ -2,14 +2,15 @@
 
 # Einige Daten über dich - korrigiere die Fehler und ergänze die fehlenden Informationen
 
-name = "Dein Name"
-alter = "20"  # Ist das korrekt formatiert?
+name = "Tobias"
+alter = 42 # Ist das korrekt formatiert?
 
 # Ergänze eine Zeile Code, um dein Lieblingstier auszugeben
 # Beispiel: lieblingstier = "Hund"
+lieblingstier = "Adler"
 
 print("Name:", name)
 print("Alter:", alter)
 
 # Ergänze dein Lieblingstier und entferne das Kommentarzeichen der nächsten Zeile, um es auszugeben
-# print("Lieblingstier:", lieblingstier)
+print("Lieblingstier:", lieblingstier)
